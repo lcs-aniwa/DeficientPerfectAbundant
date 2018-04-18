df5d5e6d194143ec23f9ee4ca62f40d9a3742e42
Niwa Ayana <aniwa@lcs-mba3092.lcs.on.ca>
2018-04-18 10:06:23 -0400

    I tried that myself

--- DeficientPerfectAbundant/main.swift
+++ DeficientPerfectAbundant/main.swift
@@ -9,9 +9,31 @@
 import Foundation
 
 // Get the user input
-var rawInput = readLine()
+
+var positiveintegerInputCount = 0
+var sum = 0 
+for positiveintegerInputCount in input {
+    
+    //Debug
+    print(positiveintegerInputCount)
+    
+    //Process
+    switch positiveintegerInputCount {
+    case "n" :
+        positiveintegerInputCount += 1
+        
+    default : break
+        break  // do anything
+    }
+    
+}
+
+if su
+
 
 // Print out the input provided
 print("You said:")
 print(rawInput)
+
+
 
